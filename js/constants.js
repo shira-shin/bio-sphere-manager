13f7b7afd5c35afd448301c2dad682ee0ba36142
shira-shin <shiratorien9837@gmail.com>
2026-01-02 00:21:19 +0900

    Add visual evolution-ready Animal class

---
+++ js/constants.js
@@ -0,0 +1,9 @@
+export const STATES = {
+    WANDER: 'WANDER',
+    DRINK: 'DRINK',
+    EAT: 'EAT',
+    MATE: 'MATE'
+};
+
+// Tile size fallback (not currently used by Animal)
+export const TILE_SIZE = 32;
